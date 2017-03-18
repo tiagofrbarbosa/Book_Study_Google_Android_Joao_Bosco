1fe856f3ad2279509ebdb9ab525b9ab8915e8acb
Tiago Barbosa <tfreirebar@gmail.com>
2017-03-18 10:59:38 -0300

    inclusão de spinner

--- BoaViagem/app/src/main/java/tech/infofun/boaviagem/GastoActivity.java
+++ BoaViagem/app/src/main/java/tech/infofun/boaviagem/GastoActivity.java
@@ -6,8 +6,10 @@ import android.app.Dialog;
 import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.Spinner;
 
 /**
  * Created by admin on 13/03/2017.
@@ -17,11 +19,13 @@ public class GastoActivity extends Activity {
 
     private int ano, mes, dia;
     private Button dataGasto;
+    private Spinner categoria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gasto);
+
         Calendar calendar = Calendar.getInstance();
         ano = calendar.get(Calendar.YEAR);
         mes = calendar.get(Calendar.MONTH);
@@ -29,6 +33,13 @@ public class GastoActivity extends Activity {
 
         dataGasto = (Button) findViewById(R.id.data);
         dataGasto.setText(dia + "/" + (mes+1) + "/" + ano);
+
+        ArrayAdapter<CharSequence> adapter =
+                ArrayAdapter.createFromResource(
+                        this, R.array.categoria_gasto,
+                        android.R.layout.simple_spinner_item);
+        categoria = (Spinner) findViewById(R.id.categoria);
+        categoria.setAdapter(adapter);
     }
 
     public void selecionarData(View view){
