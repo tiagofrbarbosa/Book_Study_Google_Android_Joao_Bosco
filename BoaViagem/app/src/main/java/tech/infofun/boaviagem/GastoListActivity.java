c54ea690df330a3a53f29f7e936def66ea135914
Tiago Barbosa <tfreirebar@gmail.com>
2017-03-19 20:54:41 -0300

    Dialog confirmação de exclusão e Progressbar

--- BoaViagem/app/src/main/java/tech/infofun/boaviagem/GastoListActivity.java
+++ BoaViagem/app/src/main/java/tech/infofun/boaviagem/GastoListActivity.java
@@ -1,30 +1,30 @@
 package tech.infofun.boaviagem;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import android.app.ListActivity;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.SimpleAdapter;
+import android.widget.SimpleAdapter.ViewBinder;
+import android.widget.TextView;
 
 /**
  * Created by tfbarbosa on 18/03/17.
  */
 
-public class GastoListActivity extends ListActivity implements AdapterView.OnItemClickListener {
+public class GastoListActivity extends ListActivity implements OnItemClickListener {
 
     private List<Map<String, Object>> gastos;
     private String dataAnterior;
@@ -43,44 +43,70 @@ public class GastoListActivity extends ListActivity implements AdapterView.OnIte
         registerForContextMenu(getListView());
     }
 
-    private List<Map<String, Object>> listarGastos(){
-        gastos = new ArrayList<Map<String, Object>>();
-
-        Map<String, Object> item =
-                new HashMap<String, Object>();
-            item.put("data", "04/02/2012");
-            item.put("descricao","Diária hotel");
-            item.put("valor","R$ 260,00");
-            item.put("categoria", R.color.categoria_hospedagem);
-            gastos.add(item);
-
-        return gastos;
-    }
-
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id){
             Map<String, Object> map = gastos.get(position);
             String descricao = (String) map.get("descricao");
-            String mensagem = "Gasto selecionado: " + descricao;
-            Toast.makeText(this,mensagem,Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,"Gasto selecionada: " + descricao,Toast.LENGTH_SHORT).show();
     }
 
 
+    private List<Map<String, Object>> listarGastos(){
+        gastos = new ArrayList<Map<String, Object>>();
 
-    private class GastoViewBinder implements SimpleAdapter.ViewBinder {
+        Map<String, Object> item = new HashMap<String, Object>();
+        item.put("data", "04/02/2012");
+        item.put("descricao", "Diária Hotel");
+        item.put("valor", "R$ 260,00");
+        item.put("categoria", R.color.categoria_hospedagem);
+        gastos.add(item);
+
+        item = new HashMap<String, Object>();
+        item.put("data", "03/02/2012");
+        item.put("descricao", "Wifi");
+        item.put("valor", "R$ 7,00");
+        item.put("categoria", R.color.categoria_outros);
+        gastos.add(item);
+
+        item = new HashMap<String, Object>();
+        item.put("data", "02/02/2012");
+        item.put("descricao", "Táxi Aeroporto - Hotel");
+        item.put("valor", "R$ 34,00");
+        item.put("categoria", R.color.categoria_transporte);
+        gastos.add(item);
+
+        item = new HashMap<String, Object>();
+        item.put("data", "02/02/2012");
+        item.put("descricao", "Sanduíche");
+        item.put("valor", "R$ 19,90");
+        item.put("categoria", R.color.categoria_alimentacao);
+        gastos.add(item);
+
+        return gastos;
+    }
+
+
+    private class GastoViewBinder implements ViewBinder {
 
         @Override
-        public boolean setViewValue(View view, Object data, String textRepresentation){
+        public boolean setViewValue(View view, Object data,
+                                    String textRepresentation) {
 
-            if(view.getId() == R.id.data){
-                if(!dataAnterior.equals(data)){
+            if (view.getId() == R.id.data) {
+                /*if (!dataAnterior.equals(data)) {*/
                     TextView textView = (TextView) view;
                     textView.setText(textRepresentation);
                     dataAnterior = textRepresentation;
                     view.setVisibility(View.VISIBLE);
-                }else{
+                /*} else {
                     view.setVisibility(View.GONE);
-                }
+                }*/
+                return true;
+            }
+
+            if (view.getId() == R.id.categoria) {
+                Integer id = (Integer) data;
+                view.setBackgroundColor(getResources().getColor(id));
                 return true;
             }
             return false;
@@ -88,7 +114,7 @@ public class GastoListActivity extends ListActivity implements AdapterView.OnIte
     }
 
     @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo){
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.gasto_menu, menu);
     }
@@ -97,7 +123,7 @@ public class GastoListActivity extends ListActivity implements AdapterView.OnIte
     public boolean onContextItemSelected(MenuItem item){
 
         if(item.getItemId() == R.id.remover){
-            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+            AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
             gastos.remove(info.position);
             getListView().invalidateViews();
             dataAnterior = "";
--- BoaViagem/app/src/main/java/tech/infofun/boaviagem/ViagemListActivity.java
+++ BoaViagem/app/src/main/java/tech/infofun/boaviagem/ViagemListActivity.java
@@ -1,47 +1,54 @@
 package tech.infofun.boaviagem;
 
-import android.app.AlertDialog;
-import android.app.ListActivity;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.TextView;
-import android.widget.Toast;
-
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ProgressBar;
+import android.widget.SimpleAdapter;
+import android.widget.SimpleAdapter.ViewBinder;
+
 
 /**
  * Created by tfbarbosa on 18/03/17.
  */
 
-public class ViagemListActivity extends ListActivity implements AdapterView.OnItemClickListener, DialogInterface.OnClickListener{
+public class ViagemListActivity extends ListActivity implements OnItemClickListener, OnClickListener{
 
     private List<Map<String, Object>> viagens;
     private AlertDialog alertDialog;
     private int viagemSelecionada;
+    private AlertDialog dialogConfirmacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
-        String[] de = {"imagem", "destino", "data", "total"};
-        int[] para = {R.id.tipoViagem, R.id.destino, R.id.data, R.id.valor};
+        String[] de = {"imagem", "destino", "data", "total", "barraProgresso"};
+        int[] para = {R.id.tipoViagem, R.id.destino, R.id.data, R.id.valor, R.id.barraProgresso};
 
         SimpleAdapter adapter = new SimpleAdapter(this, listarViagens(), R.layout.lista_viagem,de,para);
         setListAdapter(adapter);
         getListView().setOnItemClickListener(this);
 
         this.alertDialog = criaALertDialog();
+        this.dialogConfirmacao = criaDialogConfirmacao();
+        adapter.setViewBinder(new ViagemViewBinder());
     }
 
     private List<Map<String, Object>> listarViagens(){
@@ -53,6 +60,7 @@ public class ViagemListActivity extends ListActivity implements AdapterView.OnIt
                 item.put("destino", "São Paulo");
                 item.put("data","02/02/2012 a 04/02/2012");
                 item.put("total","Gasto total R$ 314,98");
+                item.put("barraProgresso",new Double[]{500.0,450.0,314.98});
                 viagens.add(item);
 
                 item = new HashMap<String, Object>();
@@ -60,10 +68,12 @@ public class ViagemListActivity extends ListActivity implements AdapterView.OnIt
                 item.put("destino","Maceió");
                 item.put("data","14/05/2012 a 22/05/2012");
                 item.put("total", "Gasto total R$ 25834,67");
+                item.put("barraProgresso",new Double[]{500.0,450.0,314.98});
                 viagens.add(item);
 
                 return viagens;
     }
+
 
 
 
@@ -80,8 +90,14 @@ public class ViagemListActivity extends ListActivity implements AdapterView.OnIt
                 startActivity(new Intent(this, GastoListActivity.class));
                 break;
             case 3:
-                viagens.remove(this.viagemSelecionada);
+                dialogConfirmacao.show();
+                break;
+            case DialogInterface.BUTTON_POSITIVE:
+                viagens.remove(viagemSelecionada);
                 getListView().invalidateViews();
+                break;
+            case DialogInterface.BUTTON_NEGATIVE:
+                dialogConfirmacao.dismiss();
                 break;
         }
     }
@@ -104,6 +120,28 @@ public class ViagemListActivity extends ListActivity implements AdapterView.OnIt
         builder.setItems(items, this);
 
         return builder.create();
+    }
+
+    private AlertDialog criaDialogConfirmacao(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.confirmacao_exclusao_viagem);
+        builder.setPositiveButton(getString(R.string.sim),this);
+        builder.setNegativeButton(getString(R.string.nao),this);
+        return builder.create();
+    }
+
+    private class ViagemViewBinder implements SimpleAdapter.ViewBinder{
+        public boolean setViewValue(View view, Object data, String textRepresentation){
+            if (view.getId() == R.id.barraProgresso){
+                Double valores[] = (Double[]) data;
+                ProgressBar progressBar = (ProgressBar) view;
+                progressBar.setMax(valores[0].intValue());
+                progressBar.setSecondaryProgress(valores[1].intValue());
+                progressBar.setProgress(valores[2].intValue());
+                return true;
+            }
+            return false;
+        }
     }
 }
 
